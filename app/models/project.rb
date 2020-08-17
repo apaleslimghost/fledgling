73934de77b9ba872b34b3cdd6f98c87b4d829ca9
bren <bren@153.io>
2020-08-17 21:51:40 +0100

    surface all tasks from subprojects

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ApplicationRecord
   has_many :tasks
   validates :title, presence: true
   has_closure_tree
+
+  def hierarchy_tasks
+    self_and_descendants.flat_map(&:tasks)
+  end
 end
