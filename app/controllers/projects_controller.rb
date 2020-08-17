00fdf520a8f01ff576f4a09207c234d97cb474f0
bren <bren@153.io>
2020-08-17 21:30:26 +0100

    creating subprojects and listing them

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   def index
-    @projects = Project.all
+    @projects = Project.roots
   end
 
   def new
@@ -8,14 +8,19 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @project = Project.new(project_params)
+    if params[:project_id]
+      parent = Project.find(params[:project_id])
+      @project = parent.children.new(project_params)
+    else
+      @project = Project.new(project_params)
+    end
 
     if @project.save
       redirect_to @project
     else
       render 'new'
-     end
     end
+  end
 
   private def project_params
     params.require(:project).permit(:title, :description)
