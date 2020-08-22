9e017a72a6d44b40dcc546080aa6e12da653fb76
bren <bren@153.io>
2020-08-22 22:31:44 +0100

    big ol shadows

--- app/javascript/components/Page.jsx
+++ app/javascript/components/Page.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import styles from './page.module.css'
 
-export default ({ title, children, ...props }) => (
+export default ({ title, header, children, ...props }) => (
     <div {...props} className={`${styles.wrapper} ${props.className || ''}`}>
       <header className={styles.header}>
+        {header}
         <h1>{title}</h1>
       </header>
 
--- app/javascript/components/Project.jsx
+++ app/javascript/components/Project.jsx
@@ -10,18 +10,19 @@ import Link from './Link'
 import colourStyle from './colour-style'
 
 export default ({ project, parent, children, tasks, subproject, newTask }) => (
-  <Page title={project.title} style={colourStyle(project.colours)}>
-      {parent && (
-          <Link to={parent}>
-            {parent.title}
-          </Link>
-      )}
-
-      { children.length > 0 && <ProjectList projects={children}/>}
-
-      <ProjectForm project={subproject} />
-
+  <Page
+    title={project.title}
+    style={colourStyle(project.colours)}
+    header={parent && (
+      <Link to={parent}>
+        {parent.title}
+      </Link>
+    )}
+  >
       { tasks.length > 0 && <TaskList tasks={tasks}/> }
       <TaskForm task={newTask} />
+
+      { children.length > 0 && <ProjectList projects={children}/>}
+      <ProjectForm project={subproject} />
     </Page>
 )
