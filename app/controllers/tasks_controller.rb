b3a080129c28804ed06fc2d4b255bf213eec5a59
bren <bren@153.io>
2020-08-23 09:46:21 +0100

    button to complete tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,19 @@
 class TasksController < ApplicationController
   def create
     @project = Project.find(params[:project_id])
-    @task = @project.tasks.create(params.require(:task).permit(:title))
+    @task = @project.tasks.create()
     redirect_to project_path(@project)
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to @task.project
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :completed)
   end
 end
--- app/javascript/components/TaskList.jsx
+++ app/javascript/components/TaskList.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import TaskForm from './TaskForm'
+import { Form, Input } from './Form'
 import styles from './task-list.module.css'
 
 export default ({ tasks, newTask }) => (
     <ul className={styles.main}>
       {tasks.map(task => (
-          <li className={styles.item} key={task.id}>
-            {task.title}
-          </li>
+        <li className={`${styles.item} ${task.completed ? styles.completed : ''}`} key={task.id}>
+          <Form model={task}>
+            <Input name='completed' type='hidden' value={!task.completed} />
+            <button type='submit'>✓</button>
+          </Form>
+          {task.title}
+        </li>
       ))}
       {newTask && (
           <li className={styles.item}><TaskForm task={newTask} /></li>
