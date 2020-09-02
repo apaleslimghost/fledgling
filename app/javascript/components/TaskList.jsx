0de84c192ac3b60c63cdb44e87c9da93d8a1f7a1
bren <bren@153.io>
2020-09-02 16:45:07 +0100

    hide completed tasks

--- app/javascript/components/TaskList.jsx
+++ app/javascript/components/TaskList.jsx
@@ -1,23 +1,42 @@
 import React from 'react'
+import partition from 'lodash.partition'
 import TaskForm from './TaskForm'
 import TaskCompleteButton from './TaskCompleteButton'
 import Link from './Link'
 import colourStyle from './colour-style'
 import styles from './task-list.module.css'
 
-export default ({ tasks, newTask, project }) => (
-    <ul className={styles.main}>
-      {tasks.map(task => (
-        <li className={styles.item} key={task.id}>
-          <TaskCompleteButton task={task} project={project} />
-          <span className={`${styles.title} ${task.completed ? styles.completed : ''}`}>{task.title}</span>
-          {task.project.id !== project.id && (
-            <Link to={task.project} className={styles.projectLink} style={colourStyle(task.project.colours)}>{task.project.title}</Link>
-          )}
-        </li>
-      ))}
-      {newTask && (
-          <li className={styles.item}><TaskForm task={newTask} /></li>
-      )}
-    </ul>
+const List = ({ tasks, project }) => (
+  <ul className={styles.list}>
+    {tasks.map(task => (
+      <li className={styles.item} key={task.id}>
+        <TaskCompleteButton task={task} project={project} />
+        <span className={`${styles.title} ${task.completed ? styles.completed : ''}`}>{task.title}</span>
+        {task.project.id !== project.id && (
+          <Link to={task.project} className={styles.projectLink} style={colourStyle(task.project.colours)}>{task.project.title}</Link>
+        )}
+      </li>
+    ))}
+  </ul>
 )
+
+export default ({ tasks, newTask, project }) => {
+  const [ incomplete, complete ] = partition(tasks, 'completed')
+
+  return (
+    <div className={styles.main}>
+      {complete.length > 0 && (
+        <List tasks={complete} project={project} />
+      )}
+      {newTask && (
+        <TaskForm task={newTask} />
+      )}
+      {incomplete.length > 0 && (
+        <details>
+          <summary>Completed tasks</summary>
+          <List tasks={incomplete} project={project} />
+        </details>
+      )}
+    </div>
+  )
+}
