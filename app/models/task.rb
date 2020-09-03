c79c845d43424b17dc126d98e7c9b53659cc46ab
bren <bren@153.io>
2020-09-03 09:00:41 +0100

    fix adding tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,4 +8,9 @@ class Task < ApplicationRecord
   end
 
   belongs_to :project
+  has_and_belongs_to_many :dependencies,
+                          class_name: 'Task',
+                          join_table: :dependencies,
+                          foreign_key: :from,
+                          association_foreign_key: :to
 end
