b5e5ed93a9a74ff38ddce5cdcd95bd843ff3155c
bren <bren@153.io>
2020-09-03 16:00:02 +0100

    consistent card styling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   def create
     @project = Project.find(params[:project_id])
-    @task = @project.tasks.create()
+    @task = @project.tasks.create(task_params)
     redirect_to project_path(@project)
   end
 
