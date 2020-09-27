8c524a7c6ef3dbe415efffc34f6cc249303c9c18
Kara Brightwell <kara@153.io>
2020-09-27 22:21:53 +0100

    incomplete login page tweaks

--- app/javascript/components/LoginForm.jsx
+++ app/javascript/components/LoginForm.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import {Form} from './Form'
+import Page from './Page'
 
 const LoginForm = () => (
+  <Page title='Log in…' style={{ '--colour-base': 'lightcyan', '--colour-gradient_start': 'lavender', '--colour-gradient_end': 'honeydew', '--colour-title': 'midnightblue' }}>
     <Form method='post' action='/sessions'>
       <input name='email' type='email' />
       <input name='password' type='password' />
 
       <button type='submit'>Log in</button>
     </Form>
+  </Page>
 )
 
 export default LoginForm
