a501c4834407b8ae44251e7b335b421ea2b0ba78
Kara Brightwell <kara@153.io>
2020-10-16 21:32:55 +0100

    logo matches project

--- app/javascript/components/Page.jsx
+++ app/javascript/components/Page.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Logo from './Logo'
 import styles from './page.module.css'
 
-export default ({ title, aux, children, ...props }) => (
+export default ({ title, aux, project, children, ...props }) => (
     <div {...props} className={`${styles.wrapper} ${props.className || ''}`}>
       <header className={styles.header}>
         <a href='/' className={styles.logo}>
-          <img alt='Fledgling' src='/logo.svg' />
+          <Logo />
         </a>
         <div className={styles.aux}>{aux}</div>
         <h1>{title}</h1>
