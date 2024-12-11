358bf33546e3f86688f261f38ec97c60090f220f
Kara Brightwell <kara@153.io>
2024-12-11 21:44:31 +0000

    remove unused link tags

--- app/root.tsx
+++ app/root.tsx
@@ -8,26 +8,12 @@ import {
   ScrollRestoration,
   useRouteLoaderData,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node"
 import {Theme, Flex, Box, Button} from '@radix-ui/themes'
 import "@radix-ui/themes/styles.css"
 import dbServer from "./lib/db.server";
 import TagTree from "~/components/tag-tree";
 import { FilePlusIcon } from "@radix-ui/react-icons";
 import 'tippy.js/dist/tippy.css';
-
-export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
 
 export async function loader() {
   return {
